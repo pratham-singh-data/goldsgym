a8d1ccaf3a14d10ce7a2a5ebe96e8828005ade78
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-07-18 18:05:26 +0530

    Banner Made

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import { Exercises } from '../components/Exercises'
+import { HeroBanner } from '../components/HeroBanner'
+import { SearchExercises } from '../components/SearchExercises'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
   )
 }
