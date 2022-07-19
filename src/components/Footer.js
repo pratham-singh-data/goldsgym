23e94e35ef34fae2753ae2b79a3c9a64c35ce8ee
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-07-19 11:32:53 +0530

    Footer Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import Logo from "../assets/images/Logo-1.png";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#fff3f3">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="Logo" width="200px" height="40px"/>
+        <Typography variant="h5" mt="10px">Gold's Gym Demo App</Typography>
+
+        <Typography variant="h5">Data from <a href="https://rapidapi.com/justin-WFnsXH_t6/api/exercisedb/" style={{textDecoration: "none", color: "#ff2625"}}>ExerciseDb</a></Typography>
+
+        <Typography variant="h5">Videos by <a href="https://rapidapi.com/h0p3rwe/api/youtube-search-and-download/" style={{textDecoration: "none", color: "#ff2625"}}>Youtube Search and Download</a></Typography>
+      </Stack>
+    </Box>
   )
 }
