026ca14d8c6c8ff284129665a17064422ca9b017
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-07-19 16:17:49 +0530

    Final Touches

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -14,12 +14,12 @@ export const SimilarExercises = ({targetMuscleExercises, equipmentExercises}) =>
 
   return (
     <Box sx={{mt: {lg: "100px", xs: "0"}}}>
-        <Typography variant="h5">Exercises that target the same muscle group</Typography>
+        <Typography variant="h4">Exercises that target the same muscle group</Typography>
         <Stack direction="row" sx={{p:"2", position: "relative"}}>
             {targetMuscleExercises.length ? <HorizontalScrollBar data={targetMuscleExercises}/> : <Loader/>}
         </Stack>
 
-        <Typography variant="h5">Exercises that use the same equipment</Typography>
+        <Typography variant="h4">Exercises that use the same equipment</Typography>
         <Stack direction="row" sx={{p:"2", position: "relative"}}>
             {targetMuscleExercises.length ? <HorizontalScrollBar data={equipmentExercises}/> : <Loader/>}
         </Stack>
